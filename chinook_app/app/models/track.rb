1f50a3f39fcf8d1eaff639a4a59f34e83eec576b
Blueibis <jennie.li.2011@gmail.com>
2017-06-13 10:42:39 -0400

    Turned track methods into scopes

--- chinook_app/app/models/track.rb
+++ chinook_app/app/models/track.rb
@@ -3,23 +3,38 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
-  def self.short
-    shorter_than(SHORT)
-  end
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%")}
 
-  def self.medium
-    longer_than(SHORT).shorter_than(LONG)
-  end
+  scope :short,   -> { shorter_than(SHORT) }
+  scope :medium,  -> { longer_than(SHORT).shorter_than(LONG) }
+  scope :long,    -> { longer_than(LONG) }
 
-  def self.long
-    longer_than(LONG)
-  end
+  # def self.short
+  #   shorter_than(SHORT)
+  # end
 
+  # def self.medium
+  #   longer_than(SHORT).shorter_than(LONG)
+  # end
+  #
+  # def self.long
+  #   longer_than(LONG)
+  # end
+  #
   def self.shorter_than(milliseconds)
+    if milliseconds < 0
+      none
+    else
     where('milliseconds < ?', milliseconds)
+    end
   end
 
   def self.longer_than(milliseconds)
+    if milliseconds < 0
+      none
+    else
       where('milliseconds >= ?', milliseconds)
+    end
   end
+
 end
